1669a2423db960970dd1eec53ccd3dd6dae3220e
Sungjin Han <meinside@meinside.dev>
2024-09-13 15:49:43 +0900

    Refactor codes and update go modules

--- gemini.go
+++ gemini.go
@@ -4,14 +4,14 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"log"
-	"strings"
-	"sync"
+	"io"
 	"time"
 
-	"github.com/gabriel-vasile/mimetype"
+	// google ai
 	"github.com/google/generative-ai-go/genai"
-	"google.golang.org/api/option"
+
+	// my libraries
+	gt "github.com/meinside/gemini-things-go"
 )
 
 const (
@@ -34,69 +34,25 @@ Respond to user messages according to the following principles:
 	generationTimeoutSeconds = 60 // 1 minute
 )
 
-type role string
-
-const (
-	roleModel role = "model"
-	roleUser  role = "user"
-)
-
 // generate with given things
 func (c *Client) generate(ctx context.Context, prompt string, files ...[]byte) (generated string, err error) {
 	ctx, cancel := context.WithTimeout(ctx, generationTimeoutSeconds*time.Second)
 	defer cancel()
 
-	var client *genai.Client
-	client, err = genai.NewClient(ctx, option.WithAPIKey(c.googleAIAPIKey))
-	if err != nil {
-		return generated, fmt.Errorf("failed to create Google AI API client: %s", err)
-	}
-	defer client.Close()
-
-	model := client.GenerativeModel(defaultGoogleAIModel)
+	gtc := gt.NewClient(c.googleAIModel, c.googleAIAPIKey)
 
 	// system instruction
-	model.SystemInstruction = &genai.Content{
-		Role: string(roleModel),
-		Parts: []genai.Part{
-			genai.Text(defaultSystemInstruction()),
-		},
-	}
+	gtc.SetSystemInstructionFunc(defaultSystemInstruction)
 
-	// safety filters (block only high)
-	model.SafetySettings = safetySettings(genai.HarmBlockThreshold(genai.HarmBlockOnlyHigh))
-
-	// prompt
-	prompts := []genai.Part{
-		genai.Text(prompt),
-	}
-
-	// files
-	fileNames := []string{}
-	var mimeType string
+	// prompt & files
+	promptFiles := []io.Reader{}
 	for _, file := range files {
-		mimeType = stripCharsetFromMimeType(mimetype.Detect(file).String())
-
-		if file, err := client.UploadFile(ctx, "", bytes.NewReader(file), &genai.UploadFileOptions{
-			MIMEType: mimeType,
-		}); err == nil {
-			prompts = append(prompts, genai.FileData{
-				MIMEType: file.MIMEType,
-				URI:      file.URI,
-			})
-
-			fileNames = append(fileNames, file.Name) // FIXME: will wait synchronously for it to become active
-		} else {
-			log.Printf("failed to upload file(%s) for prompt: %s", mimeType, err)
-		}
+		promptFiles = append(promptFiles, bytes.NewReader(file))
 	}
-
-	// FIXME: wait for all files to become active
-	waitForFiles(ctx, client, fileNames)
 
 	// generate
 	var result *genai.GenerateContentResponse
-	if result, err = model.GenerateContent(ctx, prompts...); err == nil {
+	if result, err = gtc.Generate(ctx, prompt, promptFiles); err == nil {
 		if len(result.Candidates) > 0 {
 			candidate := result.Candidates[0]
 
@@ -121,70 +77,9 @@ func (c *Client) generate(ctx context.Context, prompt string, files ...[]byte) (
 	return generated, err
 }
 
-// generate safety settings for all supported harm categories
-func safetySettings(threshold genai.HarmBlockThreshold) (settings []*genai.SafetySetting) {
-	for _, category := range []genai.HarmCategory{
-		/*
-			// categories for PaLM 2 (Legacy) models
-			genai.HarmCategoryUnspecified,
-			genai.HarmCategoryDerogatory,
-			genai.HarmCategoryToxicity,
-			genai.HarmCategoryViolence,
-			genai.HarmCategorySexual,
-			genai.HarmCategoryMedical,
-			genai.HarmCategoryDangerous,
-		*/
-
-		// all categories supported by Gemini models
-		genai.HarmCategoryHarassment,
-		genai.HarmCategoryHateSpeech,
-		genai.HarmCategorySexuallyExplicit,
-		genai.HarmCategoryDangerousContent,
-	} {
-		settings = append(settings, &genai.SafetySetting{
-			Category:  category,
-			Threshold: threshold,
-		})
-	}
-
-	return settings
-}
-
 // generate a default system instruction with given configuration
 func defaultSystemInstruction() string {
 	return fmt.Sprintf(systemInstructionFormat,
 		time.Now().Format("2006-01-02 15:04:05 (Mon) MST"),
 	)
-}
-
-// wait for all files to be active
-func waitForFiles(ctx context.Context, client *genai.Client, fileNames []string) {
-	var wg sync.WaitGroup
-	for _, fileName := range fileNames {
-		wg.Add(1)
-
-		go func(name string) {
-			for {
-				if file, err := client.GetFile(ctx, name); err == nil {
-					if file.State == genai.FileStateActive {
-						wg.Done()
-						break
-					} else {
-						time.Sleep(300 * time.Millisecond)
-					}
-				} else {
-					log.Printf("failed to get file: %s", err)
-
-					time.Sleep(300 * time.Millisecond)
-				}
-			}
-		}(fileName)
-	}
-	wg.Wait()
-}
-
-// remove trailing charset or etc. from given mime type string
-func stripCharsetFromMimeType(mimeType string) string {
-	splitted := strings.Split(mimeType, ";")
-	return strings.TrimSpace(splitted[0])
 }
---
+++ gemini_test.go
@@ -0,0 +1,26 @@
+package rf
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+)
+
+// test gemini features
+func TestGemini(t *testing.T) {
+	googleAIAPIKey := os.Getenv("API_KEY")
+
+	if googleAIAPIKey != "" {
+		client := NewClient(googleAIAPIKey, nil)
+
+		// generate
+		if generated, err := client.generate(context.TODO(), "Summarize the content of the following url: https://github.com/meinside/rss-feeds-go"); err != nil {
+			t.Errorf("failed to generate with gemini: %s", err)
+		} else {
+			log.Printf(">>> generated: %s", generated)
+		}
+	} else {
+		log.Printf("> Provide a google ai api key: 'API_KEY' as an environment variable for testing gemini features.")
+	}
+}
